fbbfa309874e134bb9ffc77a312f3fa44d532874
Nitay <93452463+VirtuallDev@users.noreply.github.com>
2021-11-27 22:30:40 +0200

    Added simple express.js frameworks. router&server.

--- newProject.sh
+++ newProject.sh
@@ -1,8 +1,17 @@
 # #!/bin/bash
 clear
 
+RESET="\e[1;0m"
 GREEN="\e[32m"
 WHITE="\e[97m"
+GRAY="\e[90m"
+BLUE="\e[94m"
+BLUEBOLD="\e[1;94m"
+
+if [ ! -d "projects/" ]; then
+    mkdir projects
+fi
+
 cd projects/
 
 projectName=$1
@@ -10,7 +19,7 @@ projectName=$1
 mkdir $projectName
 cd $projectName
 
-echo "Creating Project... Please Wait!"
+echo -e "${BLUEBOLD}Creating Project... Please Wait!"
 sleep 5
 
 clear
@@ -45,18 +54,51 @@ elif [ $2 = "pygame" ]; then
     mkdir Components
     mkdir Sprites
     cat ../templates/py/pygame.py >>game.py
-
+	pip install pygame || pip3 install pygame
+	clear
+	
 elif [ $2 = "nodejs" ]; then
     mkdir src
     touch src/app.js
     cat ../templates/nodejs/app.js >>src/app.js
     npm init -y
+	clear
+elif [ $2 = "exprouter" ]; then
+	git clone https://github.com/VirtuallDev/express-router.git/ .
+	clear
+	cd backend
+	npm install
+	echo -e "${GREEN}Installed NPM Packages on: ${WHITE}BACKEND
+	
+	
+	
+	"
+	cd ../client
+	npm install
+	echo -e "${GREEN}Installed NPM Packages on: ${WHITE}Client
+	
+	
+	${RESET}CLIENT:
+	${BLUE}cd ${GRAY}client
+	${BLUE}node ${GRAY}src/app.js
+	
+	${RESET}BACKEND:
+	${BLUE}cd ${GRAY}backend
+	${BLUE}node ${GRAY}src/app.js
+	"
+elif [ $2 = "expserver" ]; then
+	git clone https://github.com/VirtuallDev/express-server.git/ .
+	clear
+	npm install
+	echo -e "${GREEN}Installed NPM Packages on: ${WHITE}$projectName
+	
+	${RESET}To Run:
+		${BLUE}npm ${GRAY}start-server
+	"
 elif [ -z "$2" ] || [ "$2" -lt 1 ]; then
     echo "Can't find project type. Creating A blank one."
     touch README.md
     echo "# $projectName" >>README.md
 fi
-
-clear
 
 echo -e "${GREEN}Created New Project: ${WHITE}${projectName}"
